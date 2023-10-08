fa67518c1b57eb708681c644c7b8d5910cb0adf1
Sonata996 <129296155+Sonata996@users.noreply.github.com>
2023-10-08 20:33:56 +0300

    a

--- src/components/InputName/InputName.jsx
+++ src/components/InputName/InputName.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Formik, Field, Form} from 'formik';
-// import * as Yup from 'yup';
+import * as Yup from 'yup';
 import { ConteinerLabel,FormButton } from './InputName.styled';
 
 
 
-// const userSchema  = Yup.object().shape({
-//   number: Yup.number()
-//   .required('This field is required!'),
-//   name: Yup.string()
-//   .required('This field is required!')
-// })
+const userSchema  = Yup.object().shape({
+  number: Yup.number()
+  .required('This field is required!'),
+  name: Yup.string()
+  .required('This field is required!')
+})
 export const InputName = ({onCangeName}) =>{
     return (
         <Formik
@@ -19,35 +19,35 @@ export const InputName = ({onCangeName}) =>{
             number: ''
           
         }}
-        // validationSchema={userSchema}
+        validationSchema={userSchema}
 
         onSubmit={(value, action) => {
             onCangeName(value)
             action.resetForm()
         }}
       >
-        {/* {({ errors, touched }) => ( */}
+        {({ errors, touched }) => (
         <Form>
 
           <ConteinerLabel>
             <label htmlFor="name">Name</label>
             <Field id="name" name="name" placeholder="Enter a name" />
-            {/* {errors.name && touched.name ? (
+            {errors.name && touched.name ? (
                <div>{errors.name}</div>
-            ) : null} */}
+            ) : null}
           </ConteinerLabel> 
 
           <ConteinerLabel>
             <label htmlFor="number">Number</label>
             <Field type="tel" name="number" />
-            {/* {errors.number && touched.number ? (
+            {errors.number && touched.number ? (
               <div>{errors.number}</div>
-            ) : null} */}
+            ) : null}
           </ConteinerLabel>
 
           <FormButton type="submit">Submit</FormButton>
         </Form>
-        {/* )} */}
+         )} 
       </Formik>
     )
 }
